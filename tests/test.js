d9a028da9b388799c149c001b2577d5dcc692e03
Reuven V. Gonzales <reuven@virtru.com>
2013-04-02 11:13:07 -0400

    Added tests

---
+++ tests/test.js
@@ -0,0 +1,24 @@
+var assert = require('chai').assert,
+    $ = require('../lib/jquery-trigger-native');
+
+var TESTER_DIV = '<div id="tester">This is a test</div>';
+
+describe('jQuery.fn.triggerNative', function() {
+  var testerJQEl = null;
+  var testerRawEl = null;
+  beforeEach(function() {
+    testerJQEl = $(TESTER_DIV);
+    testerRawEl = testerJQEl[0];
+    $(document.body).append(testerJQEl);
+  });
+  afterEach(function() {
+    testerJQEl.remove();
+  });
+
+  it('performs a mouse event', function(done) {
+    testerRawEl.addEventListener('click', function() {
+      done();
+    });
+    testerJQEl.triggerNative('click');
+  });
+});
